fd786eb2ccc5352edabe0600c8bb64006be5e5a9
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-04-14 08:56:10 +0200

    problemas surgiendo

--- src/app/components/actors/actors.component.ts
+++ src/app/components/actors/actors.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { Actor } from '../../models/actor';
 import { FirestoreService } from '../../services/firestore.service';
@@ -25,21 +26,32 @@ export class AppComponent {
 
   recibirActorClicado(evento: Actor){
     this.actorClicado = evento;
+=======
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ACTORES } from 'src/app/models/info-actor';
+import { Actor } from 'src/app/models/actor';
+
+@Component({
+  selector: 'app-actors',
+  templateUrl: './actors.component.html',
+  styleUrls: ['./actors.component.scss']
+})
+
+
+export class ActorsComponent  {
+  actores: Actor[] = ACTORES;
+  @Input() nombre: string ="";
+  @Input() masculino: boolean =false;
+  @Input() femenino: boolean =false;
+  @Input( ) pais: string ="";
+  @Output() actorClicado: EventEmitter<number> = new EventEmitter();
+  
+  enviarActorClicado(valor: number): void {
+    this.actorClicado.emit(valor);
+>>>>>>> parent of 7130e80 (Primeros pasos con actors)
   }
 
-  getActorsFromFirestore() {
-    this.firestoreService.getDocuments('actors').subscribe((actorsData) => {
-      this.actores = actorsData.map((actorData) => {
-        const actor: Actor = {
-          nombre: actorData.nombre,
-          genero: actorData.genero,
-          nacionalidad: actorData.nacionalidad,
-          edad: actorData.edad,
-          descripcion: actorData.descripcion,
-          video: actorData.video,
-        };
-        return actor;
-      });
-    });
-  }
+
+
+
 }
